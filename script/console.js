c117363321aa16f83011b03fbfd3f7c8bc216335
Eccsx <thomasmattone83@outlook.fr>
2020-11-17 18:26:25 +0100

    :hammer: finish terminal writing look

--- script/console.js
+++ script/console.js
@@ -1,12 +1,16 @@
 // Variable
-let oldInputCount = 0;
-
+let consoleLineCount = 0;
+const console = document.getElementById('console');
+let isResponse = false;
 // Key listener
 document.addEventListener('keyup', (e) => {
   if (e.code == "Enter") {
     const input = getConsoleInput();
-    saveConsoleInput(input);
-    clearConsoleInput();
+    if (input == "why") {
+      whyCmd();
+    }
+    saveConsoleInput(input, isResponse);
+    nextConsoleInput();
   }
 });
 
@@ -14,15 +18,25 @@ function getConsoleInput() {
   return document.getElementById('console-input').value;
 }
 
-function clearConsoleInput() {
+function nextConsoleInput() {
+  isResponse = false;
   document.getElementById('console-input').value = "";
 }
 
 function saveConsoleInput(input) {
-  const console = document.getElementById('console');
   const last = document.createElement('div');
   last.className = "old-input";
   last.innerHTML = "?:\\Thomas_Mattone> " + input;
-  console.insertBefore(last, console.children[oldInputCount]);
-  oldInputCount ++;
+  console.insertBefore(last, console.children[consoleLineCount]);
+  consoleLineCount += (isResponse ? 2 : 1);
+}
+
+// Commands
+
+function whyCmd() {
+  isResponse = true;
+  const rep = document.createElement('div');
+  rep.className = "resp-input";
+  rep.innerHTML = "because.";
+  console.insertBefore(rep, console.children[consoleLineCount]);
 }
